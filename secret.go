60b8affebaeb90febe023f559160052512854a37
Dean Jackson <deanishe@deanishe.net>
2018-01-01 13:19:41 +0100

    Document Google API usage

--- secret.go
+++ secret.go
@@ -8,6 +8,16 @@
 
 package main
 
+// Google doesn't allow app keys to be stored in open-source source code.
+// The built workflow includes a key.
+//
+// If you want to hack on the source code, register your own project here:
+// https://console.developers.google.com/apis/dashboard
+//
+// Add the Google Calendar API and create credentials for a web app, with
+// http://localhost:6142 as the redirect URI.
+//
+// The workflow only requires read access.
 const secret = `
 {
   "web": {
@@ -15,9 +25,9 @@ const secret = `
       "http://localhost:61432"
     ],
     "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-    "client_id": "65151598976-4c31heuthvab00tddcfplaev96bduv25.apps.googleusercontent.com",
-    "project_id": "alfred-calendar-summary",
-    "client_secret": "YFfDSH18F_AJDpJFq-s9TrGr",
+    "client_id": "",
+    "project_id": "",
+    "client_secret": "",
     "token_uri": "https://accounts.google.com/o/oauth2/token",
     "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs"
   }
